09d25d3b4e08b19450c36a2f3947d61f266bbdbb
Michael Teuscher <70554955+mfteuscher@users.noreply.github.com>
2024-04-01 08:24:09 -0600

    set ssr false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
   extends: ['@nuxt-themes/docus'],
   devtools: { enabled: true },
-
+  ssr: false,
   modules: [
     // Remove it if you don't use Plausible analytics
     // https://github.com/nuxt-modules/plausible

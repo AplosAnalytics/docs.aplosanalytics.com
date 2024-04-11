7c1eb7cde7d26f1df30b7f96f22319b3bb30ad24
Michael Teuscher <70554955+mfteuscher@users.noreply.github.com>
2024-04-10 18:07:20 -0600

    rename to .ts, add search

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -20,6 +20,9 @@ export default defineConfig({
         ]
       }
     ],
+     search: {
+      provider: 'local'
+    },
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' }

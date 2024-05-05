ee16fd3971c95d31f9ce968853c1c73b75274dfb
Eric Wilson <eric.wilson@aplosanalytics.com>
2024-05-05 15:09:24 -0400

    ✨ feat (CodeBlock): code line selection
    - still having an issue with line numbers showing

--- docs/components/CodeBlock.vue
+++ docs/components/CodeBlock.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="`language-${langClass} vp-adaptive-theme line-numbers-mode`">
+    <span v-if="link">
+      <a v-bind:href="link" target="_blank">Full Source</a>
+    </span>
     <button title="Copy Code" class="copy"></button>
     <span class="lang">{{ lang.toLowerCase() }}</span>
 
@@ -20,61 +23,132 @@ const props = defineProps({
     type: String,
     default: "plaintext", // Default to plaintext if no language is provided
   },
+  highlightLines: {
+    type: Array,
+    default: () => [],
+  },
+  showLineNumbers: {
+    type: Boolean,
+    default: true,
+  },
+  displayLines: {
+    type: Array, // Array of integers or ranges
+    default: () => [],
+  },
+  link: {
+    type: String,
+    default: "",
+  },
 });
 
 const code = ref("");
+//const code = ref("");
 const htmlFormattedCode = ref("");
 
 const langClass = computed(() => props.lang.toLowerCase());
+const highlighter = ref(null);
+const language_comment_blocks = [
+  {
+    lang: "python",
+    start: "#",
+    end: "",
+  },
+  {
+    lang: "html",
+    start: "<!-- ",
+    end: "-->",
+  },
+  {
+    lang: "c#",
+    start: "//",
+    end: "",
+  },
+  {
+    lang: "java",
+    start: "//",
+    end: "",
+  },
+  {
+    lang: "R",
+    start: "#",
+    end: "",
+  },
+  {
+    lang: "shell",
+    start: "#",
+    end: "",
+  },
+  {
+    lang: "bash",
+    start: "#",
+    end: "",
+  },
 
+];
 // Fetch and highlight code
 async function highlightCode() {
-  try {
-    htmlFormattedCode.value = await codeToHtml(code.value, {
-      lang: props.lang,
+  const highlighter = await getHighlighter({
+    themes: ["github-dark", "github-light"],
+    langs: [props.lang],
+    showLineNumbers: props.showLineNumbers,
+  });
 
+  if (!highlighter) {
+    console.error("Highlighter not initialized.");
+    return;
+  }
+  try {
+    const validLines = props.highlightLines.filter(
+      (line) => line != null && Number.isInteger(line)
+    );
+
+    const highlightedCode = highlighter.codeToHtml(code.value, {
+      lang: props.lang,
+      lineOptions: validLines.map((line) => ({ line, classes: ["highlighted"] })),
       themes: {
         dark: "github-dark", // 'slack-dark',
         light: "github-light", //'slack-ochin',
-      
       },
-      
-      // testing line highlight, it's not working yet... i think it's a different pattern
-      // this is based on some docs but based on parameters, i don't think is correct anymore
-      // lineOptions: [
-      //     { line: 10, classes: ['highlighted']}
-      // ]
     });
-    // it's doing some light grey formatting,
-    // so i'm removing it until i can figure out where it's coming from
-    const find = [
-      "background-color:#24292e",
-      "color:#e1e4e8",
-      "background-color:#222222",
-      "color:#E6E6E6",
-      "background-color:#fff;--shiki-dark-bg:#24292e;", 
-      "color:#24292e;--shiki-dark:#e1e4e8"
-    ];
-    htmlFormattedCode.value = findAndReplace(htmlFormattedCode.value, find, "", false);
+    htmlFormattedCode.value = removeUnwantedStyles(highlightedCode);
   } catch (e) {
-    console.error(e);
+    console.error("Error highlighting code:", e);
   }
 }
 
-function findAndReplace(str, find, replace, caseSensitive = true) {
-  // If case sensitivity is not required, transform both the string and the find pattern to lowercase
-
-  for (let f in find) {
-    // console.log(f)
-    const find_str = find[f];
-    if (!caseSensitive) {
-      str = str.replace(new RegExp(escapeRegExp(find_str), "gi"), replace);
-    } else {
-      str = str.replace(new RegExp(escapeRegExp(find_str), "g"), replace);
-    }
+function wrap_comment_for_lang_text(text) {
+  console.log(props.lang);
+  const block = language_comment_blocks.find(comment => comment.lang.toLowerCase() === props.lang.toLowerCase());
+  
+  if (block) {
+    const start = block["start"]
+    const end = block["end"]
+    text = `${start}${text}${end}`
   }
+  // todo make this
 
-  return str;
+  return text;
+}
+
+function skiped_lines_text(start, end) {
+  const text = `... skipped lines [${start}-${end}] for brevity...`;
+
+  return wrap_comment_for_lang_text(text);
+}
+
+function removeUnwantedStyles(formattedCode) {
+  const unwantedStyles = [
+    "background-color:#24292e",
+    "color:#e1e4e8",
+    "background-color:#222222",
+    "color:#E6E6E6",
+    "background-color:#fff;--shiki-dark-bg:#24292e;",
+    "color:#24292e;--shiki-dark:#e1e4e8",
+  ];
+  unwantedStyles.forEach((style) => {
+    formattedCode = formattedCode.replace(new RegExp(escapeRegExp(style), "g"), "");
+  });
+  return formattedCode;
 }
 
 /**
@@ -99,11 +173,78 @@ watchEffect(() => {
   highlightCode(); // Re-highlight the code
 });
 
+function filterLines(lines, displayLines) {
+  let result = [];
+  let lastLineIndex = -2; // Adjusted to -2 to handle edge case for the first element appropriately
+
+  displayLines.forEach((item) => {
+    let currentStart = -1;
+    let currentEnd = -1;
+
+    if (typeof item === "number") {
+      currentStart = currentEnd = item - 1; // Convert 1-based to 0-based index
+    } else if (item.start && item.end) {
+      currentStart = item.start - 1;
+      currentEnd = item.end - 1;
+    }
+
+    // Check if there is a gap and insert a placeholder with skipped lines range
+    if (lastLineIndex >= 0 && currentStart > lastLineIndex + 1) {
+      const skippedStart = lastLineIndex + 2; // Convert back to 1-based and adjust to next line after last
+      const skippedEnd = currentStart; // Already 0-based, adjust to 1-based by not adding 1
+      const text = skiped_lines_text(skippedStart, skippedEnd);
+      result.push(text);
+    }
+
+    // Add lines for current start to end if they exist
+    for (let i = currentStart; i <= currentEnd && i < lines.length; i++) {
+      if (lines[i]) {
+        result.push(lines[i]);
+      }
+    }
+
+    // Update lastLineIndex to the end of the current range
+    lastLineIndex = currentEnd;
+  });
+
+  // After processing all items, check if there are any lines left to display
+  if (lastLineIndex < lines.length - 1) {
+    const skippedStart = lastLineIndex + 2; // The next line after the last included line
+    const skippedEnd = lines.length; // The last line of the document
+    const text = skiped_lines_text(skippedStart, skippedEnd);
+    result.push(text);
+  }
+
+  return result.join("\n");
+}
+
 async function fetchCode(src) {
   const response = await fetch(src);
   if (!response.ok) {
     throw new Error("Failed to fetch code");
   }
-  return await response.text();
+  const text = await response.text();
+  if (props.displayLines.length === 0) {
+    return text;
+  }
+  const lines = text.split("\n");
+  return filterLines(lines, props.displayLines);
 }
 </script>
+
+<style scoped>
+code {
+  counter-reset: step;
+  counter-increment: step 0;
+}
+
+code .line::before {
+  content: counter(step);
+  counter-increment: step;
+  width: 1rem;
+  margin-right: 1.5rem;
+  display: inline-block;
+  text-align: right;
+  color: rgba(115, 138, 148, 0.4);
+}
+</style>

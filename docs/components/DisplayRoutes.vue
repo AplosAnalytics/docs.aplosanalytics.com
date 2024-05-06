2309d5ff3904d7e5b20e0f08fa77ad369d053e2f
Eric Wilson <eric.wilson@aplosanalytics.com>
2024-05-05 23:20:06 -0400

    ⚠️ routes (notice): updates to routes before go-live

--- docs/components/DisplayRoutes.vue
+++ docs/components/DisplayRoutes.vue
@@ -47,7 +47,7 @@ const endpoints = computed(() => {
         <path
           d="M6.457 1.047c.659-1.234 2.427-1.234 3.086 0l6.082 11.378A1.75 1.75 0 0 1 14.082 15H1.918a1.75 1.75 0 0 1-1.543-2.575Zm1.763.707a.25.25 0 0 0-.44 0L1.698 13.132a.25.25 0 0 0 .22.368h12.164a.25.25 0 0 0 .22-.368Zm.53 3.996v2.5a.75.75 0 0 1-1.5 0v-2.5a.75.75 0 0 1 1.5 0ZM9 11a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z"
         ></path></svg
-      >Route Warning: Subject to change before Go-Live Summer 2024
+      >The final routes are subject to change before Go-Live Summer 2024
     </p>
     <p>The following applies to EARLY ACCESS prior to Go Live.</p>
     <p>      
@@ -56,7 +56,7 @@ const endpoints = computed(() => {
       documentation.
     </p>
     <p>
-      In order to make a smooth tranistion, we'll provide detailed information on which routes change and which remained the
+      In order to make a smooth tranistion, we'll provide detailed information on which routes changed and which remained the
       same.  As a good programming practice, you store the routes in a central library for easier migration.
     </p>
     <p>
